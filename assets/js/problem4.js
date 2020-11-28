94b72cf89c05f8ed285849d179654b3ab5d3e916
Casey Rodriguez <itscaseyr@gmail.com>
2020-11-27 20:36:54 -0500

    Reworked problem 4

--- assets/js/problem4.js
+++ assets/js/problem4.js
@@ -4,14 +4,14 @@
 
 window.setInterval(swapText, 3000);    // Run swapText every three seconds
 
-var currIndex = 0;
+var currIndex = 0; // Keep track of the index we are on
 var phraseList = ["Display alternating text using a timer or when a user clicks a button",
                   "Display text that alternates using a button or when a user clicks a timer",
                   "Text button using a display or when a user's click alternates"];
 
 function swapText() {
-    var section = document.getElementById("p4-text");
+    var section = document.getElementById("p4-text");               // Get element
 
-    section.innerText = phraseList[currIndex % phraseList.length]
-    currIndex++;
+    section.innerText = phraseList[currIndex % phraseList.length]   // Change text to the next index, taking the remainder of the index and length to be the real index
+    currIndex++;                                                    // Advance to the next index
 }
