f204e4ce542a9185464d023c60be3d0163d9a1c7
Casey Rodriguez <itscaseyr@gmail.com>
2020-11-27 19:06:00 -0500

    Resolved issue to allow for name and gen searches

--- assets/js/problem1.js
+++ assets/js/problem1.js
@@ -5,18 +5,32 @@
  */
 function filterSearch() {
     // Declare variables
-    var input = document.getElementById("myInput"); // Get input
-    var filter = input.value.toUpperCase();         // Non-case-specific searching
-    var table = document.getElementById("myTable"); // Get table
-    var tr = table.getElementsByTagName("tr");      // Get all table rows
+    var inputName = document.getElementById("myInputName");     // Get input of Name seraches
+    var inputGen = document.getElementById("myInputGenerator")  // Get input of Generator searches
+    var filterName = inputName.value.toUpperCase();             // Non-case-specific searching of names
+    var filterGen = inputGen.value.toUpperCase();               // Non-case-specific searching of generators
+    var table = document.getElementById("myTable");             // Get table
+    var tr = table.getElementsByTagName("tr");                  // Get all table rows
   
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
+        console.log(i)
+        // Check name matches
         td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
+        tdg = tr[i].getElementsByTagName("td")[1];
+        if (td || tdg) {
+            txtName = td.textContent || td.innerText;
+            txtGen = tdg.textContent || tdg.innerText;
+            console.log(txtName, txtName.toUpperCase().indexOf(filterName));
+            console.log(txtGen, txtGen.toUpperCase().indexOf(filterGen));
+            console.log(filterName,filterGen);
+            if (txtName.toUpperCase().indexOf(filterName) > -1) {
+                if(txtGen.toUpperCase().indexOf(filterGen) > -1) {
+                    tr[i].style.display = "";
+                }
+                else {
+                    tr[i].style.display = "none";
+                }
             } else {
             tr[i].style.display = "none";
             }
