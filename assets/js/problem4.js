044e111aae1a6efe86cdfeb064ebe2109dcda5ac
Casey Rodriguez <itscaseyr@gmail.com>
2020-11-27 20:18:52 -0500

    Added problem 4 functionality

--- assets/js/problem4.js
+++ assets/js/problem4.js
@@ -1,3 +1,17 @@
 /**
  * Problem #4 - Alternating Texts
  */
+
+window.setInterval(swapText, 3000);    // Run swapText every three seconds
+
+var currIndex = 0;
+var phraseList = ["Display alternating text using a timer or when a user clicks a button",
+                  "Display text that alternates using a button or when a user clicks a timer",
+                  "Text button using a display or when a user's click alternates"];
+
+function swapText() {
+    var section = document.getElementById("p4-text");
+
+    section.innerText = phraseList[currIndex % phraseList.length]
+    currIndex++;
+}
